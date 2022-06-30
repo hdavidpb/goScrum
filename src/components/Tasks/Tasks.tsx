8f91f99a9285fde8299f4a1c651ea7cf05ccdcf4
hernan plaza <hplaza@lya-electronic.com>
2022-06-30 15:48:33 -0500

    [UPDATE] Tasks view

--- src/components/Tasks/Tasks.tsx
+++ src/components/Tasks/Tasks.tsx
@@ -1,7 +1,242 @@
 import React from "react";
+import "./Tasks.styles.css";
 
 const Tasks = () => {
-  return <div>Tasks</div>;
+  return (
+    <div className="task_view_container">
+      <div className="tasks-container">
+        <h1 id="title-card">Nuevas</h1>
+        <div className="task_list_container">
+          <div className="task-card-container">
+            <div className="header-task">
+              <h6>Tarea 1</h6>
+              <span>X</span>
+            </div>
+            <span className="task-date">12-12-2020, 10:00pm</span>
+            <span className="task-userName">Pepito</span>
+            <div className="task-buttons-state">
+              <button>new</button>
+              <button>low</button>
+            </div>
+            <p className="task-description">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              veniam error nemo. Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Ratione veniam error nemo. Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Ratione veniam error nemo.
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              veniam error nemo. Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Ratione veniam error nemo. Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Ratione veniam error nemo.
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              veniam error nemo.
+            </p>
+          </div>
+          <div className="task-card-container">
+            <div className="header-task">
+              <h6>Tarea 1</h6>
+              <span>X</span>
+            </div>
+            <span className="task-date">12-12-2020, 10:00pm</span>
+            <span className="task-userName">Pepito</span>
+            <div className="task-buttons-state">
+              <button>new</button>
+              <button>low</button>
+            </div>
+            <p className="task-description">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              veniam error nemo. Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Ratione veniam error nemo. Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Ratione veniam error nemo.
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              veniam error nemo. Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Ratione veniam error nemo. Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Ratione veniam error nemo.
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              veniam error nemo.
+            </p>
+          </div>
+          <div className="task-card-container">
+            <div className="header-task">
+              <h6>Tarea 1</h6>
+              <span>X</span>
+            </div>
+            <span className="task-date">12-12-2020, 10:00pm</span>
+            <span className="task-userName">Pepito</span>
+            <div className="task-buttons-state">
+              <button>new</button>
+              <button>low</button>
+            </div>
+            <p className="task-description">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              veniam error nemo. Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Ratione veniam error nemo. Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Ratione veniam error nemo.
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              veniam error nemo. Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Ratione veniam error nemo. Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Ratione veniam error nemo.
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              veniam error nemo.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="tasks-container">
+        <h1 id="title-card">En proceso</h1>
+        <div className="task_list_container">
+          <div className="task-card-container">
+            <div className="header-task">
+              <h6>Tarea 1</h6>
+              <span>X</span>
+            </div>
+            <span className="task-date">12-12-2020, 10:00pm</span>
+            <span className="task-userName">Pepito</span>
+            <div className="task-buttons-state">
+              <button>new</button>
+              <button>low</button>
+            </div>
+            <p className="task-description">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              veniam error nemo. Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Ratione veniam error nemo. Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Ratione veniam error nemo.
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              veniam error nemo. Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Ratione veniam error nemo. Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Ratione veniam error nemo.
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              veniam error nemo.
+            </p>
+          </div>
+          <div className="task-card-container">
+            <div className="header-task">
+              <h6>Tarea 1</h6>
+              <span>X</span>
+            </div>
+            <span className="task-date">12-12-2020, 10:00pm</span>
+            <span className="task-userName">Pepito</span>
+            <div className="task-buttons-state">
+              <button>new</button>
+              <button>low</button>
+            </div>
+            <p className="task-description">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              veniam error nemo. Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Ratione veniam error nemo. Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Ratione veniam error nemo.
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              veniam error nemo. Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Ratione veniam error nemo. Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Ratione veniam error nemo.
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              veniam error nemo.
+            </p>
+          </div>
+          <div className="task-card-container">
+            <div className="header-task">
+              <h6>Tarea 1</h6>
+              <span>X</span>
+            </div>
+            <span className="task-date">12-12-2020, 10:00pm</span>
+            <span className="task-userName">Pepito</span>
+            <div className="task-buttons-state">
+              <button>new</button>
+              <button>low</button>
+            </div>
+            <p className="task-description">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              veniam error nemo. Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Ratione veniam error nemo. Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Ratione veniam error nemo.
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              veniam error nemo. Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Ratione veniam error nemo. Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Ratione veniam error nemo.
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              veniam error nemo.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="tasks-container">
+        <h1 id="title-card">Terminadas</h1>
+        <div className="task_list_container">
+          <div className="task-card-container">
+            <div className="header-task">
+              <h6>Tarea 1</h6>
+              <span>X</span>
+            </div>
+            <span className="task-date">12-12-2020, 10:00pm</span>
+            <span className="task-userName">Pepito</span>
+            <div className="task-buttons-state">
+              <button>new</button>
+              <button>low</button>
+            </div>
+            <p className="task-description">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              veniam error nemo. Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Ratione veniam error nemo. Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Ratione veniam error nemo.
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              veniam error nemo. Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Ratione veniam error nemo. Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Ratione veniam error nemo.
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              veniam error nemo.
+            </p>
+          </div>
+          <div className="task-card-container">
+            <div className="header-task">
+              <h6>Tarea 1</h6>
+              <span>X</span>
+            </div>
+            <span className="task-date">12-12-2020, 10:00pm</span>
+            <span className="task-userName">Pepito</span>
+            <div className="task-buttons-state">
+              <button>new</button>
+              <button>low</button>
+            </div>
+            <p className="task-description">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              veniam error nemo. Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Ratione veniam error nemo. Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Ratione veniam error nemo.
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              veniam error nemo. Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Ratione veniam error nemo. Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Ratione veniam error nemo.
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              veniam error nemo.
+            </p>
+          </div>
+          <div className="task-card-container">
+            <div className="header-task">
+              <h6>Tarea 1</h6>
+              <span>X</span>
+            </div>
+            <span className="task-date">12-12-2020, 10:00pm</span>
+            <span className="task-userName">Pepito</span>
+            <div className="task-buttons-state">
+              <button>new</button>
+              <button>low</button>
+            </div>
+            <p className="task-description">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              veniam error nemo. Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Ratione veniam error nemo. Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Ratione veniam error nemo.
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              veniam error nemo. Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Ratione veniam error nemo. Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Ratione veniam error nemo.
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              veniam error nemo.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Tasks;
